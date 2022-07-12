45c371a4cec9314803564fe911b569ae7b65c7ee
Pavnish Kumar Rana <rana@Pavnishs-MacBook-Pro.local>
2022-07-12 11:55:41 +0530

    fix compilation issue

--- NetworkHandler/Services/URLRequest+Convenience.swift
+++ NetworkHandler/Services/URLRequest+Convenience.swift
@@ -37,64 +37,64 @@ extension URLRequest {
     }
 }
 
-class ApiRequest<T, P> {
-  
-  var method: HTTPMethod
-  var url: String
-  var header: HTTPHeaders
-  var param: Parameters?
-  var encoding: ParameterEncoding
-  
-  required internal init(url: String,
-                         method: HTTPMethod = .get,
-                         header: [String: String]? = nil,
-                         param: [String: Any]? = nil,
-                         encoding: ParameterEncoding = URLEncoding.default) {
-    self.url = url
-    self.method = method
-    
-    self.header = header ?? [:]
-    //additional headers
-    //self.header["Content-Type"] = "application/json"
-    //self.header["Content-Type"] = "application/x-www-form-urlencoded"
-
-    self.param = param
-    self.encoding = encoding
-  }
-  
-    static func signedRequest<H, P:Encodable>(url: String,
-                            method: HTTPMethod = .get,
-                            header: H? = nil,
-                            paramModel: P? = nil,
-                            encoding: ParameterEncoding = JSONEncoding.default)
-    -> ApiRequest {
-      var header = header
-      if header == nil {
-        header = [:]
-        header?["Content-Type"] = "application/json"
-      }
-      header?["Authorization"] = "Bearer \(UserPreferences.get()?.sessionToken ?? "")"
-      header?["os"] = "ios"
-      header?["locale"] = "\(Utility.selectedLanguage)"
-      return ApiRequest(url: url, method: method, header: header,
-                        param: paramModel?.dic, encoding: encoding)
-  }
-  
-  static func unsignedRequest<>(url: String,
-                              method: HTTPMethod = .get,
-                              header: [String: String]? = nil,
-                              paramModel: Encodable? = nil,
-                              encoding: ParameterEncoding = JSONEncoding.default)
-    -> ApiRequest {
-    
-      var header = header
-      if header == nil {
-       header = [:]
-       header?["Content-Type"] = "application/json"
-      }
-      return ApiRequest(url: url, method: method, header: header,
-                        param: paramModel?.dic, encoding: encoding)
-  }
-}
+//class ApiRequest<T, P> {
+//  
+//  var method: HTTPMethod
+//  var url: String
+//  var header: HTTPHeaders
+//  var param: Parameters?
+//  var encoding: ParameterEncoding
+//  
+//  required internal init(url: String,
+//                         method: HTTPMethod = .get,
+//                         header: [String: String]? = nil,
+//                         param: [String: Any]? = nil,
+//                         encoding: ParameterEncoding = URLEncoding.default) {
+//    self.url = url
+//    self.method = method
+//    
+//    self.header = header ?? [:]
+//    //additional headers
+//    //self.header["Content-Type"] = "application/json"
+//    //self.header["Content-Type"] = "application/x-www-form-urlencoded"
+//
+//    self.param = param
+//    self.encoding = encoding
+//  }
+//  
+//    static func signedRequest<H, P:Encodable>(url: String,
+//                            method: HTTPMethod = .get,
+//                            header: H? = nil,
+//                            paramModel: P? = nil,
+//                            encoding: ParameterEncoding = JSONEncoding.default)
+//    -> ApiRequest {
+//      var header = header
+//      if header == nil {
+//        header = [:]
+//        header?["Content-Type"] = "application/json"
+//      }
+//      header?["Authorization"] = "Bearer \(UserPreferences.get()?.sessionToken ?? "")"
+//      header?["os"] = "ios"
+//      header?["locale"] = "\(Utility.selectedLanguage)"
+//      return ApiRequest(url: url, method: method, header: header,
+//                        param: paramModel?.dic, encoding: encoding)
+//  }
+//  
+//  static func unsignedRequest<>(url: String,
+//                              method: HTTPMethod = .get,
+//                              header: [String: String]? = nil,
+//                              paramModel: Encodable? = nil,
+//                              encoding: ParameterEncoding = JSONEncoding.default)
+//    -> ApiRequest {
+//    
+//      var header = header
+//      if header == nil {
+//       header = [:]
+//       header?["Content-Type"] = "application/json"
+//      }
+//      return ApiRequest(url: url, method: method, header: header,
+//                        param: paramModel?.dic, encoding: encoding)
+//  }
+//}
 
 
